79adb1071f4a01aa78984726a6732a080d0a0666
PranayShinde <pranayshinde929@gmail.com>
2025-03-08 18:54:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import './style.css';
 // Get your Gemini API key by:
 // - Selecting "Add Gemini API" in the "Project IDX" panel in the sidebar
 // - Or by visiting https://g.co/ai/idxGetGeminiKey
-let API_KEY = 'AIzaSyCZdmhH4zxw8saKSNt_UooA3RL2yKBwY3o';
+let API_KEY = '';
 
 let form = document.querySelector('form');
 let promptInput = document.querySelector('input[name="prompt"]');
